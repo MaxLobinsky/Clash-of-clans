c195ff3b05385cc22b9dcc6e3e9e79d9ed509e16
MaxLobinsky <max.lobinsky@gmail.com>
2022-09-12 10:00:33 +0300

    about updated

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,16 +11,7 @@
           {{ item.descr }}
         </template>
         <template v-slot:footer>
-          <div class="card-stats">
-            <div
-              v-for="(stat, index) in item.info"
-              :key="index"
-              class="one-third"
-            >
-              <div class="stat-value">{{ stat.value }}</div>
-              <div class="stat">{{ stat.title }}</div>
-            </div>
-          </div>
+          <CardFooter :item="item" />
         </template>
       </Card>
     </slide>
@@ -33,10 +24,12 @@
 <script>
 import items from "@/seeders/items.js";
 import Card from "@/components/UI/Card.vue";
+import CardFooter from "@/components/UI/CardFooter.vue";
 import "vue3-carousel/dist/carousel.css";
 import { Carousel, Slide, Navigation } from "vue3-carousel";
 export default {
   components: {
+    CardFooter,
     Card,
     Carousel,
     Slide,
